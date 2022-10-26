8aaebc7a6c0366f728890071d66470fcacafe118
matheus_brandao <matheusvongola@yahoo.com>
2022-10-26 19:38:34 -0300

    'Update'

---
+++ App/arboVirose.py
@@ -0,0 +1,146 @@
+import os
+import urllib.request
+import pandas as pd
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+
+def initDriver(name):
+    if name == ("google-chrome.desktop" or "brave-browser.desktop" or "chromium_chromium.desktop"):
+        options = ChromeOptions()
+        options.add_argument("--headless")
+
+        if name == "google-chrome.desktop":
+            driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+        elif name == "brave-browser.desktop":
+            driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()), options=options)
+        elif name == "chromium_chromium.desktop":
+            driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=options)
+
+    elif (name == "firefox.desktop"):
+        options = FirefoxOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install(), log_path=os.devnull), options=options)
+
+    return driver
+
+class arboVirose():
+    def __init__(self, arbo_v):
+        os.environ['WDM_LOG_LEVEL'] = '0'
+        self.arbo_v = arbo_v
+    
+    def obterUrl(self):
+        if self.arbo_v == 'dengue':
+            return "http://www3.saude.ba.gov.br/cgi/deftohtm.exe?sinan/deng.def"
+        elif self.arbo_v == 'chikungunya':
+            return "http://www3.saude.ba.gov.br/cgi/deftohtm.exe?sinan/chikun.def"
+        elif self.arbo_v == 'zika':
+            return "http://www3.saude.ba.gov.br/cgi/deftohtm.exe?sinan/zika.def"
+    
+    def obterInfo(self):
+        html = urllib.request.urlopen(self.obterUrl())
+        soup = BeautifulSoup(html, 'html5lib')
+
+        linha = soup.find("div", {"class": "linha"}).find("select").select("option")
+        linha = list(map(lambda node: node.get_text().strip(), linha))
+
+        coluna = soup.find("div", {"class": "coluna"}).find("select").select("option")
+        coluna = list(map(lambda node: node.get_text().strip(), coluna))
+
+        periodo = soup.find("div", {"class": "periodo"}).find("select").select("option")
+        periodo = list(map(lambda node: node.get_text().strip(), periodo))
+        
+        self.linha, self.coluna, self.periodo = linha, coluna, periodo
+    
+    def obterDf(self, linha_valor, coluna_valor, periodo_valor):
+        browser_name = ["google-chrome.desktop", "brave-browser.desktop", "chromium_chromium.desktop"]
+        i = 0
+        while (i < 3):
+            try:
+                driver = initDriver(browser_name[i])
+                i = 3
+            except:
+                i += 1
+
+        driver.set_page_load_timeout(100)
+        driver.get(self.obterUrl())
+
+        WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.CLASS_NAME, 'mostra')))
+
+        if self.arbo_v == "dengue" or self.arbo_v == "chikungunya":
+            # Linha
+            driver.find_element(By.XPATH, f'/html/body/div/center/div/form/div[2]/div/div[1]/select/option[{self.linha.index(linha_valor)+1}]').click()
+            # Coluna
+            driver.find_element(By.XPATH, f'/html/body/div/center/div/form/div[2]/div/div[2]/select/option[{self.coluna.index(coluna_valor)+1}]').click()
+            # Periodo
+            driver.find_element(By.XPATH, '/html/body/div/center/div/form/div[3]/div/select/option[1]').click()
+            for i, item in enumerate(self.periodo):
+                if item in periodo_valor:
+                    driver.find_element(By.XPATH, f'/html/body/div/center/div/form/div[3]/div/select/option[{i+1}]').click()
+            # Mostra
+            driver.find_element(By.XPATH, '/html/body/div/center/div/form/div[4]/div[2]/div[2]/input[1]').click()
+        else:
+            # Linha
+            driver.find_element(By.XPATH, f'/html/body/center/div/form/div[2]/div/div[1]/select/option[{self.linha.index(linha_valor)+1}]').click()
+            # Coluna
+            driver.find_element(By.XPATH, f'/html/body/center/div/form/div[2]/div/div[2]/select/option[{self.coluna.index(coluna_valor)+1}]').click()
+            # Periodo
+            driver.find_element(By.XPATH, '/html/body/center/div/form/div[3]/div/select/option[1]').click()
+            for i, item in enumerate(self.periodo):
+                if item in periodo_valor:
+                    driver.find_element(By.XPATH, f'/html/body/center/div/form/div[3]/div/select/option[{i+1}]').click()
+
+            # Mostra
+            driver.find_element(By.XPATH, '/html/body/center/div/form/div[4]/div[2]/div[2]/input[1]').click()
+
+        html = driver.page_source
+        driver.quit()
+        
+        soup = BeautifulSoup(html, 'html.parser')
+        tabdados = soup.select(".tabdados tbody tr td ")
+        tabdados = list(map(lambda node: node.get_text().strip(), tabdados))
+        col_tabdados = soup.select(".tabdados th ")
+        col_tabdados = list(map(lambda node: node.get_text().strip(), col_tabdados))
+        
+        tam_lin = int(len(tabdados)/len(col_tabdados))
+        tam_col = len(col_tabdados)
+
+        aux = []
+        for i in range (tam_lin):
+            aux_2 = []
+            for j in range (tam_col):  
+                aux_2.append(tabdados.pop(0))
+            aux.append(aux_2)
+
+        df = pd.DataFrame(aux, columns=col_tabdados)
+        df = df.drop(columns=['Total'])
+        df = df.drop(0)
+        df = df.reset_index()
+        df = df.drop(columns=['index'])
+        
+        aux = []
+        for item in df.iloc[:, 0]:
+            aux.append(item.split(" ", 1))
+
+        aux = pd.DataFrame(aux)
+
+        df["Código"] = aux[0]
+        df["Município"] = aux[1]
+
+        aux = list(df.columns)
+        aux.insert(0, aux.pop())
+        df = df[aux]
+        
+        df.iloc[:, 2:] = df.iloc[:, 2:].astype(str).replace('\.', '', regex=True)
+        df.iloc[:, 2:] = df.iloc[:, 2:].astype(str).replace('-', '0')
+        
+        return df
---
+++ App/plot.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+class plotGraficos():
+    def __init__(self, df, arbo_v, linha, coluna, periodo):
+        plt.rcParams["figure.figsize"] = (15, 15)
+        self.df, self.arbo_v, self.linha, self.coluna, self.periodo = df, arbo_v, linha, coluna, periodo
+
+    def definirPlotagem(self):
+        if self.coluna == 'Ano da Notificação':
+            self.pltCasos()
+        elif self.coluna == 'Ano do Óbito':
+            self.pltObitos()
+        elif self.coluna == 'Mês da Notificação':
+            self.pltMes()
+        elif self.coluna == 'Sexo':
+            self.pltSexo()
+        elif self.coluna == 'Fx Etária SINAN':
+            self.pltFxEtaria()
+
+    def pltCasos(self):
+        casos = self.df.iloc[:, 2:].astype('int64').sum()
+        casos = pd.DataFrame(casos.values.tolist(), index = casos.index.to_list(), columns=["Casos"])
+        plt.bar(casos.index, casos.iloc[:, 0], color="#103b6e")
+        plt.xlabel("Anos")
+        plt.ylabel("Casos")
+        if self.arbo_v == "dengue":
+            plt.title("Dengue")
+        elif self.arbo_v == "chikungunya":
+            plt.title("Chikungunya")
+        elif self.arbo_v == "zika":
+            plt.title("Zika Vírus")
+          
+    def pltObitos(self):
+        obitos = self.df.iloc[:, 2:].astype('int64').sum()
+        obitos = pd.DataFrame(obitos.values.tolist(), index = obitos.index.to_list(), columns=["Óbitos"])
+        plt.bar(obitos.index, obitos.iloc[:, 0], color="#103b6e")
+        plt.xlabel("Anos")
+        plt.ylabel("Óbitos")
+        plt.title("Dengue")
+        
+    def pltMes(self):
+        meses = self.df.iloc[:, 2:].astype('int64').sum()
+        meses = pd.DataFrame(meses.values.tolist(), index = meses.index.to_list(), columns=["Meses"])
+        plt.bar(meses.index, meses.iloc[:, 0], color="#103b6e")
+        plt.xlabel("Meses")
+        plt.ylabel("Casos")
+        if self.arbo_v == "dengue":
+            plt.title(f"Dengue")
+        elif self.arbo_v == "chikungunya":
+            plt.title(f"Chikungunya")
+        elif self.arbo_v == "zika":
+            plt.title("Zika Vírus")
+            
+    def pltSexo(self):
+        sexo = self.df.iloc[:, 2:].astype('int64')
+        sexo["Branco e Ignorado"] = sexo["Em Branco"] + sexo["Ignorado"]
+        sexo = sexo.drop(columns=['Em Branco'])
+        sexo = sexo.drop(columns=['Ignorado'])
+        sexo = sexo.sum()
+        sexo = pd.DataFrame(sexo.values.tolist(), index = sexo.index.to_list(), columns=["Sexo"])
+        plt.bar(sexo.index, sexo.iloc[:, 0], color="#103b6e")
+        plt.xlabel("Sexos")
+        plt.ylabel("Casos")
+        if self.arbo_v == "dengue":
+            plt.title(f"Dengue")
+        elif self.arbo_v == "chikungunya":
+            plt.title(f"Chikungunya")
+        elif self.arbo_v == "zika":
+            plt.title("Zika Vírus")
+            
+    def pltFxEtaria(self):
+        fx_etaria = self.df.iloc[:, 2:].astype('int64').sum()
+        fx_etaria = pd.DataFrame(fx_etaria.values.tolist(), index = fx_etaria.index.to_list(), columns=["Faixa Etária"])
+
+        Faixa = ["Menor que 1", "Entre 1 e 9", "Entre 10 e 19", "Entre 20 e 34", "Entre 35 e 49", "Entre 50 e 64", "Entre 65 e 79", "Maior que 80"]
+        Casos = [fx_etaria.loc["<1 Ano"][0], fx_etaria.loc["1-4"][0] + fx_etaria.loc["5-9"][0], fx_etaria.loc["10-14"][0] + fx_etaria.loc["15-19"][0], fx_etaria.loc["20-34"][0], fx_etaria.loc["35-49"][0], fx_etaria.loc["50-64"][0], fx_etaria.loc["65-79"][0], fx_etaria.loc["80 e+"][0]]
+
+        plt.bar(Faixa, Casos, color="#103b6e")
+        plt.xlabel("Faixas")
+        plt.ylabel("Casos")
+        if self.arbo_v == "dengue":
+            plt.title(f"Dengue")
+        elif self.arbo_v == "chikungunya":
+            plt.title(f"Chikungunya")
+        elif self.arbo_v == "zika":
+            plt.title("Zika Vírus")
